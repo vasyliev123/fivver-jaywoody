5ea616bae693cb116679663d21773f789eafa6f0
vasyliev123 <vasylievdmytro04@gmail.com>
2022-10-11 11:21:34 -0700

    init commit

--- fiverr.py
+++ fiverr.py
@@ -12,117 +12,117 @@ client5 = TelegramClient('anon5', api_id, api_hash)
 client6 = TelegramClient('anon6', api_id, api_hash)
 client7= TelegramClient('anon7', api_id, api_hash)
 
-# async def main1():
+async def main1():
 
-#     #FIVERR001
-#     groups = []
-#     async for dialog in client1.iter_dialogs():
-#         if(dialog.id < 0):
-#                 print(dialog.name)
-#                 groups.append(dialog.id)
-#     print(groups)      
-#     for group in groups:
-#         try:
-#             await client1.send_file(group, 'pic1.jpg')
-#             print("message sent to group" + str(group))
-#         except Exception as er:
-#             print(er)
+    #FIVERR001
+    groups = []
+    async for dialog in client1.iter_dialogs():
+        if(dialog.id < 0):
+                print(dialog.name)
+                groups.append(dialog.id)
+    print(groups)      
+    for group in groups:
+        try:
+            await client1.send_file(group, 'pic1.jpg')
+            print("message sent to group" + str(group))
+        except Exception as er:
+            print(er)
 
-# async def main2():
-#     #FIVERR002
-#     groups = []
-#     async for dialog in client2.iter_dialogs():
-#         if(dialog.id < 0):
-#                 print(dialog.name)
-#                 groups.append(dialog.id)
-#     print(groups)      
-#     for group in groups:
-#         try:
-#             await client2.send_message(group, '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
-#             '\n'+
-#             '-Recover any account within 0-60 minutes✅\n'+
-#             "-Only need your @ 🙏\n"+
-#             '-All types of ban reasons except perm bans 🔨\n'+
-#             '-100% vetted and guaranteed to work💖\n'+
-#             '-Any account size🔥\n'+
-#             '-MM accepted❕\n'+
-#             '\n'+
-#             '-Plenty of testimonials and proof of work Just message me for more info🙌')
-#             print("message sent to group" + str(group))
-#             time.sleep(1)
-#         except Exception as er:
-#             print(er)
+async def main2():
+    #FIVERR002
+    groups = []
+    async for dialog in client2.iter_dialogs():
+        if(dialog.id < 0):
+                print(dialog.name)
+                groups.append(dialog.id)
+    print(groups)      
+    for group in groups:
+        try:
+            await client2.send_message(group, '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
+            '\n'+
+            '-Recover any account within 0-60 minutes✅\n'+
+            "-Only need your @ 🙏\n"+
+            '-All types of ban reasons except perm bans 🔨\n'+
+            '-100% vetted and guaranteed to work💖\n'+
+            '-Any account size🔥\n'+
+            '-MM accepted❕\n'+
+            '\n'+
+            '-Plenty of testimonials and proof of work Just message me for more info🙌')
+            print("message sent to group" + str(group))
+            time.sleep(1)
+        except Exception as er:
+            print(er)
 
-# async def main3():    
-#     #FIVERR003
-#     groups = []
-#     async for dialog in client3.iter_dialogs():
-#         if(dialog.id < 0):
-#                 print(dialog.name)
-#                 groups.append(dialog.id)
-#     print(groups)      
-#     for group in groups:
-#         try:
-#             await client3.send_file(group,'pic3.jpg',caption=
-#             'DM me now to book🥰\n'+
-#             "-quality fans🔥\n"+
-#             '-never spam with shoutouts or s4s🔥✅\n'+
-#             '80k fans❤️‍🔥')
-#             print("message sent to group" + str(group))
-#             time.sleep(1)
-#         except Exception as er:
-#             print(er)
+async def main3():    
+    #FIVERR003
+    groups = []
+    async for dialog in client3.iter_dialogs():
+        if(dialog.id < 0):
+                print(dialog.name)
+                groups.append(dialog.id)
+    print(groups)      
+    for group in groups:
+        try:
+            await client3.send_file(group,'pic3.jpg',caption=
+            'DM me now to book🥰\n'+
+            "-quality fans🔥\n"+
+            '-never spam with shoutouts or s4s🔥✅\n'+
+            '80k fans❤️‍🔥')
+            print("message sent to group" + str(group))
+            time.sleep(1)
+        except Exception as er:
+            print(er)
 
-# async def main4():
-#     #FIVERR004
-#     groups = []
-#     async for dialog in client4.iter_dialogs():
-#         if(dialog.id < 0):
-#                 print(dialog.name)
-#                 groups.append(dialog.id)
-#     print(groups)      
-#     for group in groups:
-#         try:
-#             await client4.send_message(group, '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
-#             '\n'+
-#             '-Recover any account within 0-60 minutes✅\n'+
-#             "-Only need your @ 🙏\n"+
-#             '-All types of ban reasons except perm bans 🔨\n'+
-#             '-100% vetted and guaranteed to work💖\n'+
-#             '-Any account size🔥\n'+
-#             '-MM accepted❕\n'+
-#             '\n'+
-#             '-Plenty of testimonials and proof of work Just message me for more info🙌')
-#             print("message sent to group" + str(group))
-#             time.sleep(1)
-#         except Exception as er:
-#             print(er)
+async def main4():
+    #FIVERR004
+    groups = []
+    async for dialog in client4.iter_dialogs():
+        if(dialog.id < 0):
+                print(dialog.name)
+                groups.append(dialog.id)
+    print(groups)      
+    for group in groups:
+        try:
+            await client4.send_message(group, '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
+            '\n'+
+            '-Recover any account within 0-60 minutes✅\n'+
+            "-Only need your @ 🙏\n"+
+            '-All types of ban reasons except perm bans 🔨\n'+
+            '-100% vetted and guaranteed to work💖\n'+
+            '-Any account size🔥\n'+
+            '-MM accepted❕\n'+
+            '\n'+
+            '-Plenty of testimonials and proof of work Just message me for more info🙌')
+            print("message sent to group" + str(group))
+            time.sleep(1)
+        except Exception as er:
+            print(er)
 
 
-# async def main6():
-#     #FIVERR006
-#     groups = []
-#     async for dialog in client6.iter_dialogs():
-#         if(dialog.id < 0):
-#                 print(dialog.name + " "+ str(dialog.id))
-#                 groups.append(dialog.id)
-#     print(groups)      
-#     for group in groups:
-#         try:
-#             if group != -1001859762702:
-#                 await client6.send_file(group, 'pic6.jpg',caption = '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
-#                 '\n'+
-#                 '-Recover any account within 0-60 minutes✅\n'+
-#                 "-Only need your @ 🙏\n"+
-#                 '-All types of ban reasons except perm bans 🔨\n'+
-#                 '-Negotiable prices💰\n'+
-#                 'Send me a message\n'+
-#                 '@baddiprincess❤️‍🔥\n'+
-#                 '@baddiprincess❤️‍🔥')
-#                 print("message sent to group" + str(group))
-#                 time.sleep(1)
-#         except Exception as er:
-#             print(er)
+async def main6():
+    #FIVERR006
+    groups = []
+    async for dialog in client6.iter_dialogs():
+        if(dialog.id < 0):
+                print(dialog.name + " "+ str(dialog.id))
+                groups.append(dialog.id)
+    print(groups)      
+    for group in groups:
+        try:
+            if group != -1001859762702:
+                await client6.send_file(group, 'pic6.jpg',caption = '!!UNBAN ANY INSTAGRAM ACCOUNT!!\n'+
+                '\n'+
+                '-Recover any account within 0-60 minutes✅\n'+
+                "-Only need your @ 🙏\n"+
+                '-All types of ban reasons except perm bans 🔨\n'+
+                '-Negotiable prices💰\n'+
+                'Send me a message\n'+
+                '@baddiprincess❤️‍🔥\n'+
+                '@baddiprincess❤️‍🔥')
+                print("message sent to group" + str(group))
+                time.sleep(1)
+        except Exception as er:
+            print(er)
 
 async def main5():
     #FIVERR005
@@ -173,18 +173,18 @@ async def main7():
        
 
 def init():   
-    # with client1:
-    #     client1.loop.run_until_complete(main1())
-    # with client2:
-    #     client2.loop.run_until_complete(main2())
-    # with client3:
-    #     client3.loop.run_until_complete(main3())
-    # with client4:
-    #     client4.loop.run_until_complete(main4())
+    with client1:
+        client1.loop.run_until_complete(main1())
+    with client2:
+        client2.loop.run_until_complete(main2())
+    with client3:
+        client3.loop.run_until_complete(main3())
+    with client4:
+        client4.loop.run_until_complete(main4())
     with client5:
         client5.loop.run_until_complete(main5())
-    # with client6:
-    #     client6.loop.run_until_complete(main6())
+    with client6:
+        client6.loop.run_until_complete(main6())
     with client7:
         client7.loop.run_until_complete(main7())
 
